65a883dce5b2a355a3484b506440169147add5a0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-02 14:00:57 +0000

    Deploy renato-bohler/logossim to github.com/renato-bohler/logossim.git:gh-pages

---
+++ precache-manifest.76ad75fd9d7dc95d838029c1df697f0a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "763cae2c5682e111cfd1fcdbec8f85bc",
+    "url": "https://renato-bohler.github.io/logossim/SimulationWorker.acd14983af5ba5608049.js"
+  },
+  {
+    "revision": "a28e434319e5559946ec48fc83d0cd6e",
+    "url": "https://renato-bohler.github.io/logossim/index.html"
+  },
+  {
+    "revision": "147e7ffdf7b47c8c0739",
+    "url": "https://renato-bohler.github.io/logossim/static/css/2.fbd218b0.chunk.css"
+  },
+  {
+    "revision": "eef99c8a09f1f6b59c2c",
+    "url": "https://renato-bohler.github.io/logossim/static/css/main.1f618b3c.chunk.css"
+  },
+  {
+    "revision": "147e7ffdf7b47c8c0739",
+    "url": "https://renato-bohler.github.io/logossim/static/js/2.9bc58dfe.chunk.js"
+  },
+  {
+    "revision": "31a4d2df3f0709713d80e46668251aa9",
+    "url": "https://renato-bohler.github.io/logossim/static/js/2.9bc58dfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef99c8a09f1f6b59c2c",
+    "url": "https://renato-bohler.github.io/logossim/static/js/main.36d9585c.chunk.js"
+  },
+  {
+    "revision": "b085145bd191f95114b2",
+    "url": "https://renato-bohler.github.io/logossim/static/js/runtime-main.044d442d.js"
+  },
+  {
+    "revision": "e3ec32b9b22d17eeb2eadc5e9b5cbc36",
+    "url": "https://renato-bohler.github.io/logossim/static/media/bifurcation.e3ec32b9.gif"
+  },
+  {
+    "revision": "94343183837e1c43b9be51ed5d5a2da2",
+    "url": "https://renato-bohler.github.io/logossim/static/media/context-menu.94343183.gif"
+  },
+  {
+    "revision": "2d0f910c2cb97460e4cd900c425eed3f",
+    "url": "https://renato-bohler.github.io/logossim/static/media/simulation.2d0f910c.gif"
+  },
+  {
+    "revision": "e7e521dab809f1b350e19eab61bdff4b",
+    "url": "https://renato-bohler.github.io/logossim/static/media/wire.e7e521da.gif"
+  }
+]);
